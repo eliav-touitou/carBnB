6117cd1bcd12e2efffd8a35940133a01ece59862
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-06-14 22:44:33 +0300

    fixed register route

--- backend/routes/v1/users.js
+++ backend/routes/v1/users.js
@@ -2,25 +2,25 @@ const { Router } = require("express");
 const users = Router();
 const {
   getAllItems,
-  getItem,
+  getUserOrAuth,
   addToUsersDB,
   addToAuthDB,
-} = require("../../../database");
-const { User } = require("../../../database/models");
+} = require("../../../database/queries");
+const { User, Auth } = require("../../../database/models");
 const { hashSync, genSaltSync, compareSync } = require("bcrypt");
 const { sign } = require("jsonwebtoken");
 
 // Gets a unique user
 users.post("/uniqueuser", async (req, res) => {
-  const { id } = req.body;
-  console.log(id);
+  const { email } = req.body;
+  console.log(email);
   try {
-    const user = await getItem("users", "user_id", id);
+    const user = await getUserOrAuth(User, email);
     console.log(user);
     res.status(200).json({ success: true, data: user });
-  } catch (err) {
-    console.log(err.message);
-    res.status(404).json({ message: "NOT FOUND", error: err.message });
+  } catch (error) {
+    console.log(error.message);
+    res.status(404).json({ message: "NOT FOUND", error: error.message });
   }
 });
 
@@ -30,33 +30,34 @@ users.get("/allusers", async (req, res) => {
     const users = await getAllItems(User);
     console.log(users);
     res.status(200).json({ success: true, data: users });
-  } catch (err) {
-    console.log(err.message);
-    res.status(404).json({ message: "NOT FOUND", error: err.message });
+  } catch (error) {
+    console.log(error.message);
+    res.status(404).json({ message: "NOT FOUND", error: error.message });
   }
 });
 
-// Create new user
+// Create new auth and user
 users.post("/register", async (req, res) => {
   try {
     const { newUser } = req.body;
-    const { firstName, lastName, address, phoneNumber, email } = newUser;
+    const { firstName, lastName, address, phoneNumber, email, password } =
+      newUser;
+    const auth = await getUserOrAuth(Auth, email);
 
-    if (!(await getItem("auth", "email", email))) {
-      newUser.password = hashSync(newUser.password, genSaltSync(10));
+    if (!auth) {
+      newAuthObj.password = hashSync(newUser.password, genSaltSync(10));
+
       await addToUsersDB({ firstName, lastName, address, phoneNumber, email });
-      await addToAuthDB({
-        password: newUser.password,
-        email,
-        firstName,
-        lastName,
-      });
+      await addToAuthDB({ password, email, firstName, lastName });
+      newUser.password = undefined;
+
       res.status(200).json({ success: true, data: newUser });
     } else {
       res.status(409).json({ success: true, message: "User already exist" });
     }
-  } catch (err) {
-    res.status(500).json({ message: "NOT FOUND", error: err.message });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: "NOT FOUND", error: error.message });
   }
 });
 
