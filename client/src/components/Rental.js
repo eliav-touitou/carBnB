d4af389a0f497752ba4d1a9772b312c0bf7a1b5d
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-06-15 20:27:14 +0300

    added option to upload image
    change the file to binary

---
+++ client/src/components/Rental.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+export default function Rental() {
+  const [image, setImage] = useState("");
+  const imageToBinary = async (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      console.log("Encoded Base 64 File String:", reader.result);
+      const data = reader.result.split(",")[1];
+      const binaryBlob = atob(data);
+      setImage(data);
+      console.log("Encoded Binary File String:", binaryBlob);
+    };
+    reader.readAsDataURL(file);
+  };
+
+  const SaveImage = async () => {
+    // table: User,
+    //   column: ["first_name", "last_name"],
+    //   primaryKey: "user_email",
+    //   primaryKeyValue: "eliav@gmail.com",
+    //   content: ["mose", "levy"],
+
+    try {
+      const arr = ["User", ["license"], "eliav@gmail.com", [image]];
+      await axios.post("api/v1/users/updateitems", { data: arr });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  return (
+    <div>
+      <div>
+        <input type="file" onChange={(e) => imageToBinary(e)}></input>
+      </div>
+      <button onClick={SaveImage}>upload image</button>
+      <img src={`data:image/jpeg;base64,${image}`} />
+    </div>
+  );
+}
