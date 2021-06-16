3a0ac8d49e1d9e2293375f78ac83bfa60b7d2f1a
eliav-touitou <77244738+eliav-touitou@users.noreply.github.com>
2021-06-16 10:02:52 +0300

    added search by car brand

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,5 +1,31 @@
-import React from "react";
+import axios from "axios";
+import React, { useRef } from "react";
 
 export default function SearchBar() {
-  return <div></div>;
+  const brandRef = useRef();
+  const search = async () => {
+    const brand = brandRef.current.value;
+    const searchParameters = {
+      data: ["Car", ["brand"], [`${brand}`]],
+    };
+    try {
+      await axios.post("api/v1/getitem", searchParameters);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+  return (
+    <div>
+      do you know what car do you want?:
+      <input list="cars" ref={brandRef}></input>
+      <datalist id="cars">
+        <option value="BMW" />
+        <option value="Bentley" />
+        <option value="Mercedes" />
+        <option value="Audi" />
+        <option value="Volkswagen" />
+      </datalist>
+      <button onClick={search}>search!</button>
+    </div>
+  );
 }
