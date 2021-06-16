4adacb4a7c90653318b8aa71e1cc231773dc36e6
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-06-16 14:17:28 +0300

    added 3 queries to DB

--- database/queries.js
+++ database/queries.js
@@ -118,6 +118,62 @@ const updateItemToDB = async (obj) => {
   }
 };
 
+const getByCity = async (obj) => {
+  const { address } = obj;
+  try {
+    return await User.findAll({
+      where: { address: { [Op.like]: `%${address}%` } },
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getExistCars = async (obj) => {
+  const arrOfEmails = obj.emails;
+  const numOfPassengers = obj.passengers;
+  const startDate = obj.dates.start;
+  const endDate = obj.dates.end;
+
+  const result = await Car.findAll({
+    where: {
+      [Op.and]: {
+        available_from: {
+          [Op.lte]: startDate,
+        },
+
+        available_until: {
+          [Op.gte]: endDate,
+        },
+
+        passengers: { [Op.gte]: numOfPassengers },
+        owner_email: { [Op.in]: arrOfEmails },
+      },
+    },
+  });
+  return result;
+};
+
+const whatCarsAreTaken = async (obj) => {
+  const arrOfCarsId = obj.carsId;
+  const startDate = obj.dates.start;
+  const endDate = obj.dates.end;
+
+  Rental.findAll({
+    where: {
+      [Op.and]: {
+        car_id: { [Op.in]: arrOfCarsId },
+        start_date: {
+          [Op.lte]: startDate,
+        },
+        end_date: {
+          [Op.gte]: endDate,
+        },
+      },
+    },
+  });
+};
+
 module.exports = {
   getCar,
   getRental,
@@ -127,4 +183,7 @@ module.exports = {
   addToUsersDB,
   addToAuthDB,
   updateItemToDB,
+  getByCity,
+  getExistCars,
+  whatCarsAreTaken,
 };
