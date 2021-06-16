44e4efe67168e972e885eb83e4a1d107ec96221f
eliav-touitou <77244738+eliav-touitou@users.noreply.github.com>
2021-06-16 12:15:26 +0300

    added initial search component

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -2,14 +2,20 @@ import axios from "axios";
 import React, { useRef } from "react";
 
 export default function SearchBar() {
-  const brandRef = useRef();
+  const cityRef = useRef();
+  const passengersRef = useRef();
+  const endDateRef = useRef();
+  const startDateRef = useRef();
   const search = async () => {
-    const brand = brandRef.current.value;
+    const city = cityRef.current.value;
+    const passengers = passengersRef.current.value;
+    const startDate = startDateRef.current.value;
+    const endDate = endDateRef.current.value;
     const searchParameters = {
-      data: ["Car", ["brand"], [`${brand}`]],
+      data: [city, startDate, endDate, passengers],
     };
     try {
-      const res = await axios.post("api/v1/getitem", searchParameters);
+      const res = await axios.post("api/v1/search/initial", searchParameters);
       console.log(res);
     } catch (err) {
       console.log(err.message);
@@ -17,25 +23,52 @@ export default function SearchBar() {
   };
   return (
     <div>
-      do you know what car do you want?:
-      <input list="cars" ref={brandRef}></input>
-      <datalist id="cars">
-        <option value="Reno" />
-        <option value="Suzuki" />
-        <option value="Toyota" />
-        <option value="Ford" />
-        <option value="Skoda" />
-      </datalist>
-      <button onClick={search}>search!</button>
-      do you what size of car you need?
-      <input list="passengers" ref={brandRef}></input>
-      <datalist id="passengers">
-        <option value="Reno" />
-        <option value="Suzuki" />
-        <option value="Toyota" />
-        <option value="Ford" />
-        <option value="Skoda" />
-      </datalist>
+      <div className="choose-city">
+        Where do you want a car?:
+        <input list="Cities" ref={cityRef}></input>
+        <datalist id="Cities">
+          <option value="Modiin" />
+          <option value="Efrat" />
+          <option value="Pisgat_zeev" />
+          <option value="cfar haroee" />
+          <option value="Yokneam" />
+        </datalist>
+      </div>
+      <div className="choose-dates">
+        <div className="start-date">
+          <label for="start">Start date:</label>
+          <input
+            ref={startDateRef}
+            type="date"
+            name="rent-start"
+            // value={new Date()}
+            min="2018-01-01"
+            max="2022-01-01"
+          ></input>
+        </div>
+        <div className="end-date">
+          <label for="start">End date:</label>
+          <input
+            ref={endDateRef}
+            type="date"
+            name="rent-end"
+            // value="2018-07-22"
+            min="2018-01-01"
+            max="2022-02-01"
+          ></input>
+        </div>
+      </div>
+      <div>
+        what size of car you need?
+        <input list="passengers" ref={passengersRef}></input>
+        <datalist id="passengers">
+          <option value="2+" />
+          <option value="4+" />
+          <option value="5+" />
+          <option value="7+" />
+          <option value="else..." />
+        </datalist>
+      </div>
       <button onClick={search}>search!</button>
     </div>
   );
