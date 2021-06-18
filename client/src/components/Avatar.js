e6e96a88b4e53f8ac0d16f559d54ce1fd6a3607b
Jino1211 <77382695+Jino1211@users.noreply.github.com>
2021-06-18 09:12:08 +0300

    added avatar component
    and redirect to home page after login

---
+++ client/src/components/Avatar.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Avatar as ProfileAvatar, ClickAwayListener } from "@material-ui/core";
+import { useSelector, useDispatch } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import { setAuthOut } from "../actions";
+import axios from "axios";
+
+export default function Avatar() {
+  const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth);
+  const [open, setOpen] = useState(false);
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      position: "relative",
+    },
+    dropdown: {
+      position: "absolute",
+      top: 28,
+      right: 0,
+      left: 0,
+      zIndex: 1,
+      border: "1px solid",
+      padding: theme.spacing(1),
+      width: "min-content",
+      backgroundColor: theme.palette.background.paper,
+    },
+  }));
+
+  const classes = useStyles();
+
+  const handleClick = () => {
+    setOpen((prev) => !prev);
+  };
+
+  const handleClickAway = () => {
+    setOpen(false);
+  };
+
+  const logoutHandler = async () => {
+    try {
+      console.log(await axios.post("/api/v1/users/logout"));
+      dispatch(setAuthOut());
+    } catch (error) {
+      console.log("error logout");
+    }
+  };
+
+  return (
+    <div>
+      <ClickAwayListener onClickAway={handleClickAway}>
+        <div className={classes.root}>
+          <button type="button" onClick={handleClick}>
+            <ProfileAvatar>{auth.first_name?.slice(0, 1)}</ProfileAvatar>
+          </button>
+          {open ? (
+            <div>
+              <Link to="/Profile">
+                <div className={classes.dropdown}>Profile</div>
+              </Link>
+              <div className={classes.dropdown} onClick={logoutHandler}>
+                Logout
+              </div>
+            </div>
+          ) : null}
+        </div>
+      </ClickAwayListener>
+    </div>
+  );
+}
