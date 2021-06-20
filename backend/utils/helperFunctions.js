ca89916b83d92cf8f25190c23a817db6b78df4e1
Jino1211 <77382695+Jino1211@users.noreply.github.com>
2021-06-20 19:04:57 +0300

    Add route for new rantal
    and send emails to owner car and the renter

--- backend/utils/helperFunctions.js
+++ backend/utils/helperFunctions.js
@@ -4,4 +4,20 @@ const calculateDiscount = (tariff, percent, period) => {
   return newPrice;
 };
 
-module.exports = { calculateDiscount };
+const buildPatterns = ({ transactionId, startDate, endDate }) => {
+  const textPatternToRenter = `Dear valued User,
+  Your order Num ${transactionId} was successfully made!
+  you will be notified when the car owner will approve your request. 
+  in case the car owner won't handle your request within 12 hours the order will be canceled and you will be notified.
+  kind regards, carBnB team`;
+
+  const textPatternToOwner = `Dear valued User,
+  your car got an order from user.... on these dates: ${startDate} - ${endDate}.
+  please enter your account to confirm the order.
+  in case you won't handle the request within 12 hours the order will be canceled.
+  kind regards, carBnB team
+  `;
+
+  return { textPatternToRenter, textPatternToOwner };
+};
+module.exports = { calculateDiscount, buildPatterns };
