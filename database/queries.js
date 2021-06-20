19de1051133ad992737ad333e268a0ab04d1e840
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-06-20 19:19:49 +0300

    added option to new rental

--- database/queries.js
+++ database/queries.js
@@ -252,8 +252,7 @@ const addNewRentalToDB = async (obj) => {
     total_price: obj.totalPrice,
     is_active: true,
   };
-  await Rental.create(objToSave);
-  return objToSave;
+  return await Rental.create(objToSave);
 };
 
 module.exports = {
