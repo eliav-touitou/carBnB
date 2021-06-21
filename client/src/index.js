c81655018caa1165b3a8c5721769fa34301ffa2b
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-06-21 11:44:09 +0300

    added redux persist states

--- client/src/index.js
+++ client/src/index.js
@@ -5,16 +5,32 @@ import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import allReducers from "./reducers";
 import { applyMiddleware, compose, createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+import { PersistGate } from "redux-persist/integration/react";
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, allReducers);
+
 // STORE
-const store = createStore(allReducers, composeEnhancer(applyMiddleware(thunk)));
+const store = createStore(
+  persistedReducer,
+  composeEnhancer(applyMiddleware(thunk))
+);
+export const persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
