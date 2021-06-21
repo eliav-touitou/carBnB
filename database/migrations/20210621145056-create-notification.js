850a983770f760996f7e9defe1d3e3e21c8f6de3
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-06-21 18:03:15 +0300

    added new table to DB

---
+++ database/migrations/20210621145056-create-notification.js
@@ -0,0 +1,39 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("notifications", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      message_from: {
+        type: Sequelize.STRING,
+      },
+      message_to: {
+        type: Sequelize.STRING,
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
+      content: {
+        type: Sequelize.STRING,
+      },
+      read: {
+        type: Sequelize.BOOLEAN,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("notifications");
+  },
+};
