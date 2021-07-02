293b5e91e8850f06bd216ae7e02e3101581ec6c7
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-07-02 17:31:01 +0300

    function to build pattern of finish order

--- backend/utils/helperFunctions.js
+++ backend/utils/helperFunctions.js
@@ -17,15 +17,15 @@ const buildPatterns = ({ transactionId, startDate, endDate }) => {
   Your order Num ${transactionId} was successfully made!
   you will be notified when the car owner will approve your request. 
   in case the car owner won't handle your request within 12 hours the order will be canceled and you will be notified.
-  kind regards, carBnB team
-  </p>`;
+  kind regards, carBnB team</p>`;
 
   const textPatternToOwner = `<p>Dear valued User,
-  your car got an order from user.... on these dates: ${startDate.toDateString()} - ${endDate.toDateString()}.
+  your car got an order from user.... on these dates: ${new Date(
+    startDate
+  ).toDateString()} - ${new Date(endDate).toDateString()}.
   please enter your account to confirm the order.
   in case you won't handle the request within 12 hours the order will be canceled.
-  kind regards, carBnB team</p>
-  `;
+  kind regards, carBnB team</p>`;
 
   return { textPatternToRenter, textPatternToOwner };
 };
@@ -34,12 +34,21 @@ const buildPatternsForCanceledRentals = ({ transactionId }) => {
   const textToCanceledRenter = `<p>Dear valued User,
   Your order Num ${transactionId} was canceled because the cars owner didn't complete the process after 12 hours.
   we deeply apologize for the inconvenience
-  kind regards, carBnB team<p/>`;
+  kind regards, carBnB team</p>`;
   const textToCanceledOwner = `<p>Dear valued User,
   the order Num ${transactionId}, for your car was canceled because you didn't complete the process and approved the order within our 12H policy .
-  kind regards, carBnB team<p/>`;
+  kind regards, carBnB team</p>`;
 
   return { textToCanceledRenter, textToCanceledOwner };
+};
+
+const buildPatternsForAfterRentalFinish = ({ transactionId }) => {
+  const textToRenterAfterFinish = `<p>Dear valued User,
+  Your order Num ${transactionId} was over. Let us know how was your ride and please 
+  rating the owner for better service.
+  kind regards, carBnB team</p>`;
+
+  return { textToRenterAfterFinish };
 };
 
 const sendMail = ({ from, to, subject, text }) => {
@@ -263,4 +272,5 @@ module.exports = {
   sendMail,
   createPDFToSend,
   buildInvoice,
+  buildPatternsForAfterRentalFinish,
 };
