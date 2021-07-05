6900da3df080a6c2542725ddbd9e7e77a1e1a9ff
eliav-touitou <77244738+eliav-touitou@users.noreply.github.com>
2021-07-05 19:29:26 +0300

    Update FavoriteDetails.js

--- client/src/components/FavoriteDetails.js
+++ client/src/components/FavoriteDetails.js
@@ -1,5 +1,4 @@
 import { Snackbar } from "@material-ui/core";
-import axios from "axios";
 import React, { useState } from "react";
 import { DateRangePicker } from "react-dates";
 import { Redirect } from "react-router-dom";
