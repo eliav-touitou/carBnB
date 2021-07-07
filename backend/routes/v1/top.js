1389e3ea5175b9f8c8509bd942d46f4e4d1ee182
Jino1211 <77382695+Jino1211@users.noreply.github.com>
2021-07-07 18:07:06 +0300

    added top route

---
+++ backend/routes/v1/top.js
@@ -0,0 +1,39 @@
+const { Router } = require("express");
+const { writeLogs } = require("../../utils/helperFunctions");
+const top = Router();
+const { top5Owners } = require("../../../database/queries");
+
+top.get("/:type", async (req, res) => {
+  const { type } = req.params;
+  let topFive;
+  try {
+    switch (type) {
+      case "cars":
+        topFive = await Top5Cars(type);
+        break;
+      case "owners":
+        topFive = await top5Owners(type);
+        break;
+      case "locations":
+        topFive = await top5Locations(type);
+        break;
+      default:
+        break;
+    }
+    return res.status(200).json({ success: true, data: topFive });
+  } catch (err) {
+    const objToWrite = {
+      date: new Date(),
+      error: error,
+      status: 500,
+      ourMessage: "Problems with our server",
+      route: `api/v1/top/${type}`,
+    };
+    await writeLogs(objToWrite);
+    return res
+      .status(500)
+      .json({ message: "Problems with our server", error: error.message });
+  }
+});
+
+module.exports = top;
